ca390b34f9eb022b73c164ef9efa62dc2af3a332
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-11-07 15:22:35 +0200

    Add getHumanChoice function and score variables

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0 
+let computerScore = 0
+
 function getComputerChoice(){
   let random = Math.random()
 
@@ -11,3 +14,10 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+  let choice = prompt('paper, rock or scissors');
+  return choice
+}
+
+console.log(getHumanChoice())
