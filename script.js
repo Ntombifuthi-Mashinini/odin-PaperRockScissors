62c505bd2528813f84042709d98095dd213f86d0
Ntombifuthi Mashinini <ntombifuthimashinini4@gmail.com>
2025-11-07 15:11:20 +0200

    Set up project structure and getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('we are here')
+function getComputerChoice(){
+  let random = Math.random()
+
+  if (random < 0.33){
+    return "rock"
+  }else if(random < 0.67){
+    return "paper"
+  }else{
+    return "scissors"
+  }
+}
+
+console.log(getComputerChoice());
